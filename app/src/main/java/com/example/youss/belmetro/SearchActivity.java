b4b8365d644ae607204f346e866107b2864d386e
youssefwagih <youssefwagih@hotmail.com>
2016-04-30 01:45:50 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/youss/belmetro/SearchActivity.java
@@ -0,0 +1,53 @@
+package com.example.youss.belmetro;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+/**
+ * Created by youssef wagih on 4/13/2016.
+ */
+public class SearchActivity extends Activity implements AdapterView.OnItemSelectedListener {
+Button belMetroButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        belMetroButton = (Button) findViewById(R.id.belMetroButton);
+        Spinner spinner = (Spinner) findViewById(R.id.planets_spinner);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.planets_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        belMetroButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SearchActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
+}
