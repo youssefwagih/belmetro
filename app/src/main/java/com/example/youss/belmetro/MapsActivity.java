6c36a85e883b15d0344550b51e6c68908aa784b6
youssefwagih <youssefwagih@hotmail.com>
2016-05-15 20:01:53 +0200

    Add nearest station for testing user location (function)

--- app/src/main/java/com/example/youss/belmetro/MapsActivity.java
+++ app/src/main/java/com/example/youss/belmetro/MapsActivity.java
@@ -1,14 +1,20 @@
 package com.example.youss.belmetro;
 
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -42,5 +48,45 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        // User Location
+        LatLng currentLocation = new LatLng(30.089720, 31.298159);
+
+        // Stations Locations
+        LatLng sarayobba = new LatLng(30.097705, 31.304484);
+        LatLng hamamatobba = new LatLng(30.091263, 31.298905);
+        LatLng kobriobba = new LatLng(30.087230, 31.294136);
+
+        ArrayList<LatLng> stationsList = new ArrayList<LatLng>();
+        stationsList.add(sarayobba);
+        stationsList.add(hamamatobba);
+        stationsList.add(kobriobba);
+
+
+        mMap.addMarker(new MarkerOptions().position(sarayobba).title("sarayobba"));
+        mMap.addMarker(new MarkerOptions().position(hamamatobba).title("hamamatobba"));
+        mMap.addMarker(new MarkerOptions().position(kobriobba).title("kobriobba"));
+
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(sarayobba, 12);
+        mMap.animateCamera(update);
+        mMap.addPolyline((new PolylineOptions())
+                .add(sarayobba, hamamatobba, kobriobba).width(6).color(Color.BLUE)
+                .visible(true));
+
+        // Get Nearest Station from user location
+        LatLng nearestStation = null;
+        double nearestDistance = 10000000000000.0;
+        for (LatLng currentStationLocation : stationsList){
+            double userStationDistance = Helpers.CalculationByDistance(currentLocation, currentStationLocation);
+            if (userStationDistance < nearestDistance){
+                nearestDistance = userStationDistance;
+                nearestStation = currentStationLocation;
+            }
+        }
+
+        mMap.addPolyline((new PolylineOptions())
+                .add(currentLocation, nearestStation).width(6).color(Color.GREEN)
+                .visible(true));
+
     }
 }
--- app/src/main/java/com/example/youss/belmetro/SearchActivity.java
+++ app/src/main/java/com/example/youss/belmetro/SearchActivity.java
@@ -3,17 +3,22 @@ package com.example.youss.belmetro;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
+import com.google.android.gms.maps.model.LatLng;
+
+import java.text.DecimalFormat;
+
 /**
  * Created by youssef wagih on 4/13/2016.
  */
 public class SearchActivity extends Activity implements AdapterView.OnItemSelectedListener {
-Button belMetroButton;
+    Button belMetroButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,5 +54,31 @@ Button belMetroButton;
 
     public void onNothingSelected(AdapterView<?> parent) {
         // Another interface callback
+    }
+
+    // Get distance between 2 co-ordinates
+    public double CalculationByDistance(LatLng StartP, LatLng EndP) {
+        int Radius = 6371;// radius of earth in Km
+        double lat1 = StartP.latitude;
+        double lat2 = EndP.latitude;
+        double lon1 = StartP.longitude;
+        double lon2 = EndP.longitude;
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLon = Math.toRadians(lon2 - lon1);
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+                + Math.cos(Math.toRadians(lat1))
+                * Math.cos(Math.toRadians(lat2)) * Math.sin(dLon / 2)
+                * Math.sin(dLon / 2);
+        double c = 2 * Math.asin(Math.sqrt(a));
+        double valueResult = Radius * c;
+        double km = valueResult / 1;
+        DecimalFormat newFormat = new DecimalFormat("####");
+        int kmInDec = Integer.valueOf(newFormat.format(km));
+        double meter = valueResult % 1000;
+        int meterInDec = Integer.valueOf(newFormat.format(meter));
+        Log.i("Radius Value", "" + valueResult + "   KM  " + kmInDec
+                + " Meter   " + meterInDec);
+
+        return Radius * c;
     }
 }
